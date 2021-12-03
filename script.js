7d4d6ef2f5157984bfa903d0f101bf983d2ebd65
mchercherry <mk10733@cabrini.edu>
2021-12-02 20:07:39 -0500

    based update
    hehehe >:3

--- script.js
+++ script.js
@@ -14,25 +14,4 @@ var flkty = new Flickity( '.main-carousel', {
   // options
 });
 
-var page = 0;
-var whee = 0;
 
-const today = document.querySelector('#donthurtme');
-const log = scrollY;
-
-document.onscroll = update;
-
-document.addEventListener('scroll', function(e) {
-  update();
-});
-
-function update(e) {
-  var whee = log;
-  if (whee = 5000) {
-    var page = 3
-  };
-}
-
-//if (page = 3) {
-//  window.location.href = "palm.html"
-//};
