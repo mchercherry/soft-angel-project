4b92e04e7b6663d0c724ef7b104ee5765ce250a4
mchercherry <mk10733@cabrini.edu>
2021-12-06 00:52:49 -0500

    More Story + Added Modal
    Added modal to base site to make looking at the lil map much easier, plus additional content to story and added imagery

--- script.js
+++ script.js
@@ -14,4 +14,35 @@ var flkty = new Flickity( '.main-carousel', {
   // options
 });
 
+//------------modal moment------------
 
+//Get modal element
+const modal = document.querySelector('.mapbig');
+//Get open modal button
+const modalBtn = document.querySelector('#map');
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+
+// Listen for outside click
+window.addEventListener('click', clickOutside);
+
+//Function to open modal
+function openModal(){
+  var expand = modal;
+  expand.classList.add("visible");
+}
+
+//Function to close modal
+function closeModal(){
+  var expand = modal;
+  expand.classList.remove("visible");
+}
+
+//Function to close modal if outside click
+function clickOutside(e){
+    if(e.target == modal){
+      var expand = modal;
+      expand.classList.remove("visible");
+    }
+}
